b1bee3b86fe4ac7868da5a8fac398621028f641e
L200170018 <34600649+L200170018@users.noreply.github.com>
2019-05-20 21:30:06 +0700

    Add files via upload

---
+++ Modul_7/1.py
@@ -0,0 +1,10 @@
+#Riyan Aldiansyah
+#L200170018
+#Nomor 1
+import re
+f=open('Indonesia.txt','r', encoding='latin1')
+teks = f.read()
+f.close()
+p=r'me\w+'
+string = re.findall(p,teks)
+print(string)
---
+++ Modul_7/2.py
@@ -0,0 +1,10 @@
+#Nomor 2
+import re
+f=open('Indonesia.txt','r', encoding='latin1')
+teks2 = f.read()
+f.close()
+pola2 = r"di\w+"
+tampilan2 = re.findall(pola2,teks2)
+print(tampilan2)
+
+
